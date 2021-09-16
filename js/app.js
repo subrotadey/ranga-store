e26c7feb81b418e06f7dfae409a10b7d735f0d7f
Subrota  Dey <78078777+subrotadey@users.noreply.github.com>
2021-09-16 23:21:09 +0600

    correction

--- js/app.js
+++ js/app.js
@@ -31,8 +31,8 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-  updateTotal();
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
